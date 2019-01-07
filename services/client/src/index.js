843a99d77e8e060dfe55205566549e62ad2619f9
SidneyNiccolson <sidneyniccolson@gmail.com>
2019-01-07 18:01:03 +0100

    prep for routing

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,62 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
-import AddUser from './components/AddUser';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = { email: '', password: ''};
-    this.addUser = this.registerUser.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
+import App from './App.jsx';
 
-  componentDidMount() {
-  };
-
-  handleChange(event) {
-  const obj = {};
-  obj[event.target.name] = event.target.value;
-  this.setState(obj);
-  };
-
-  registerUser(event) {
-    event.preventDefault();
-    console.log('sanity check!');
-    const data = {
-      email: this.state.email, password: this.state.password
-    };
-    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users` , data)
-  .then(function (response) {
-    console.log(response.data.message);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-  }
-
-  render() {
-    return (
-      <section className="section">
-        <div className="container">
-          <div className="columns">
-            <div className="column is-one-third">
-              <br/>
-              <h1 className="title is-1">Register to AP</h1>
-              <hr/><br/>
-              <AddUser email={this.state.email} password={this.state.password}
-                handleChange={this.handleChange} addUser={this.addUser}/>
-            </div>
-          </div>
-        </div>
-      </section>
-    )
-  }
-};
-
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router>
+  <App />
+  </Router>
+), document.getElementById('root'))
